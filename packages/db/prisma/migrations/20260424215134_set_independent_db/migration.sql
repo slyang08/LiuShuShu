a78eebd6a78284e02cc24a580f141f707fd109a3
slyang08 <e2223845@gmail.com>
2026-05-03 17:25:32 -0400

    refactor: create test db and prod db

--- packages/db/prisma/migrations/20260424215134_set_independent_db/migration.sql
+++
@@ -1,2 +0,0 @@
--- RenameIndex
-ALTER INDEX "Admin_username_key" RENAME TO "unique_username";
--- packages/db/prisma/migrations/20260503211144_init/migration.sql
+++ packages/db/prisma/migrations/20260503211144_init/migration.sql
@@ -1,21 +1,5 @@
--- CreateTable
-CREATE TABLE "Store" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Inventory" (
-    "id" SERIAL NOT NULL,
-    "date" DATE NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "storeId" INTEGER NOT NULL,
-
-    CONSTRAINT "Inventory_pkey" PRIMARY KEY ("id")
-);
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'STAFF');
 
 -- CreateTable
 CREATE TABLE "DurianVariety" (
@@ -24,8 +8,20 @@ CREATE TABLE "DurianVariety" (
     "desc" TEXT,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "DurianVariety_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Inventory" (
+    "id" SERIAL NOT NULL,
+    "date" DATE NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "storeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Inventory_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -33,6 +29,7 @@ CREATE TABLE "InventoryItem" (
     "id" SERIAL NOT NULL,
     "quantity" INTEGER NOT NULL DEFAULT 0,
     "price" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
     "inventoryId" INTEGER NOT NULL,
@@ -41,17 +38,38 @@ CREATE TABLE "InventoryItem" (
     CONSTRAINT "InventoryItem_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'ADMIN',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "storeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
-CREATE UNIQUE INDEX "Store_name_key" ON "Store"("name");
+CREATE UNIQUE INDEX "DurianVariety_name_key" ON "DurianVariety"("name");
 
 -- CreateIndex
 CREATE INDEX "Inventory_storeId_idx" ON "Inventory"("storeId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Inventory_storeId_date_key" ON "Inventory"("storeId", "date");
-
--- CreateIndex
-CREATE UNIQUE INDEX "DurianVariety_name_key" ON "DurianVariety"("name");
 
 -- CreateIndex
 CREATE INDEX "InventoryItem_inventoryId_idx" ON "InventoryItem"("inventoryId");
@@ -62,6 +80,15 @@ CREATE INDEX "InventoryItem_varietyId_idx" ON "InventoryItem"("varietyId");
 -- CreateIndex
 CREATE UNIQUE INDEX "InventoryItem_inventoryId_varietyId_key" ON "InventoryItem"("inventoryId", "varietyId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Store_name_key" ON "Store"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "unique_username" ON "Admin"("username");
+
 -- AddForeignKey
 ALTER TABLE "Inventory" ADD CONSTRAINT "Inventory_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -70,3 +97,6 @@ ALTER TABLE "InventoryItem" ADD CONSTRAINT "InventoryItem_inventoryId_fkey" FORE
 
 -- AddForeignKey
 ALTER TABLE "InventoryItem" ADD CONSTRAINT "InventoryItem_varietyId_fkey" FOREIGN KEY ("varietyId") REFERENCES "DurianVariety"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Admin" ADD CONSTRAINT "Admin_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
