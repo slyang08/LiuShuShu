f0dfe36783fd76a912422474a28f25256057aaf1
slyang08 <e2223845@gmail.com>
2026-05-04 01:35:37 -0400

    refactor: remove express from project

--- apps/api/src/routes/healthRoutes.ts
+++
@@ -1,8 +0,0 @@
-// apps/api/src/routes/healthRoutes.ts
-import * as healthController from "../controllers/healthController";
-import { Router } from "express";
-
-const router: Router = Router();
-router.get("/", healthController.healthCheck);
-
-export default router;
--- apps/api/src/routes/storeRoutes.ts
+++
@@ -1,9 +0,0 @@
-// apps/api/src/routes/storeRoutes.ts
-import * as storeController from "../controllers/storeController";
-import { Router } from "express";
-
-const router: Router = Router();
-
-router.post("/", storeController.createStore);
-
-export default router;
